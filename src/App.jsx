5cb622bb95571f0950877aeb516debad8fa85528
Deepan Raj G <deepanrajd01@gmail.com>
2024-03-29 18:17:28 +0530

    addItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/header';
 import Footer from './Components/footer';
 import Content from './Components/content';
+import AddItem from './Components/addItem';
 // import './index.css';
 
 const App = () => {
@@ -27,6 +28,15 @@ const App = () => {
       ]
   );
 
+  const[newItem, setNewItem] = useState('')
+
+  const addItem = (item) =>{
+    const id = items.length ? items[items.length-1].id+1 : 1 ;
+    const NewItem = {id, checked:false, item};
+    setItem([...items, NewItem])
+  }
+
+
   const handleSelect = (id) => {
       setItem(items.map(item => item.id === id ? {...item, checked: !item.checked} : item));
   }
@@ -34,9 +44,21 @@ const App = () => {
   const handleDelete = (id) => {
       setItem(items.filter((item)=> item.id !== id))
   }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!newItem) return;
+    addItem(newItem)
+    setNewItem('')
+  }
   return (
     <div>
       <Header tittle="TO-DO List"/>
+      <AddItem
+      newItem = {newItem}
+      setNewItem = {setNewItem}
+      handleSubmit = {handleSubmit}
+      />
       <Content 
       items = {items}
       handleSelect = {handleSelect}
